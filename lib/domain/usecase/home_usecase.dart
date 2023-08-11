b99ef4a094cb0e0b5311df589b785caa570deb02
fadyZaherEng <fady33zaher@gmail.com>
2023-08-11 23:43:49 +0300

    upload finish

--- lib/domain/usecase/home_usecase.dart
+++ lib/domain/usecase/home_usecase.dart
@@ -3,7 +3,7 @@ import 'package:most_popular_articales/data/network/failure.dart';
 import 'package:most_popular_articales/domain/models/most_poplar_articles_response/PopularArticlesResponse.dart';
 import 'package:most_popular_articales/domain/repository/repository.dart';
 import 'package:most_popular_articales/domain/usecase/base_usecase.dart';
-
+//debendency injection principles
 class HomeUseCase implements BaseUseCase<void, PopularArticlesResponse> {
   final Repository _repository;
   HomeUseCase(this._repository);
